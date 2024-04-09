7c939d053881e8fa7f8bd9f6582322c3366ef915
Armaan Batra <armaanandroid14@gmail.com>
2024-04-09 17:37:12 +0530

    Testing the encryption, decryption and evaluation of decrypted payload
    Signed-off-by: Armaan Batra <armaanandroid14@gmail.com>

--- src/encryption_test.py
+++ src/encryption_test.py
@@ -5,7 +5,7 @@ def main():
     key = os.urandom(32)  # Generate a random key (AES key size is 256 bits)
     nonce = os.urandom(16)  # Generate a random nonce (GCM nonce size is 96 bits)
 
-    payload = b'This is a test payload.'  # Example payload
+    payload = b'a=5\nb=10\nc=15\nprint(a+b+c)'
     
     encrypted_payload, tag = encrypt_data(key, nonce, payload)
     decrypted_payload = decrypt_data(key, nonce, encrypted_payload, tag)
@@ -13,6 +13,7 @@ def main():
     print("Original Payload:", payload)
     print("Encrypted Payload:", encrypted_payload)
     print("Decrypted Payload:", decrypted_payload.decode('utf-8'))
+    exec(decrypted_payload)
 
 
 if __name__ == "__main__":
